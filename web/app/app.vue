15a1a2d56e92950a76dd39990a4f474211e25473
alsoasnerd <alsoasnerd@gmail.com>
2026-02-04 22:53:35 -0300

    feat(web): create input for user url (#3)

--- web/app/app.vue
+++ web/app/app.vue
@@ -1,6 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+
+const userUrl = ref('');
+const output = ref('');
+
+const submitUrl = () => {
+  output.value = `Shortening: ${userUrl.value}`;
+}
+
+</script>
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Shorl</h1>
+    <input v-model="userUrl">
+    <button @click="submitUrl">Short it</button>
+    <p>{{ output }}</p>
   </div>
 </template>
